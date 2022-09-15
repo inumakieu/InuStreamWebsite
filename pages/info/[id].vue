010cf18cdc2f19332562bc55cdedb30123f6d7ef
Ryusaki <kempcompany05@gmail.com>
2022-09-15 19:23:59 +0200

    added extra info tab

--- pages/info/[id].vue
+++ pages/info/[id].vue
@@ -1,29 +1,79 @@
 <template>
-    <div class="body">
-        <img class="cover-image" :src="(animeInfo.cover as string)" alt="">
-        <div class="cover-gradient"></div>
-        <div class="info-wrapper">
-            <div class="top-wrapper">
-                <div class="poster-wrapper">
-                    <img class="poster-image" :src="animeInfo.image" alt="">
-                </div>
-                <div class="title-wrapper">
-                    <h1 class="english-title">{{ (animeInfo.title as ITitle).english }}</h1>
-                    <h1 class="native-title">{{ (animeInfo.title as ITitle).native }}</h1>
-                </div>
-            </div>
-            <p class="description" v-html="animeInfo.description"></p>
-            <h1 class="episode-text">Episodes</h1>
-            <div class="episode-list">
-                <div class="episode-wrapper" v-for="episode in episodeList" v-on:click="goToEpisode(episode)">
-                    <img class="episode-image" :src="(episode.image as string)" alt="">
-                    <div class="episode-info">
-                        <h2 class="episode-title">{{ episode.title }}</h2>
-                        <h2 class="episode-number">Episode {{ episode.number }}</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
+	<div class="body">
+		<img class="cover-image" :src="(animeInfo.cover as string)" alt="">
+		<div class="cover-gradient"></div>
+		<div class="info-wrapper">
+			<div class="top-wrapper">
+				<div class="poster-wrapper">
+					<img class="poster-image" :src="animeInfo.image" alt="">
+				</div>
+				<div class="title-wrapper">
+					<h1 class="english-title">{{ (animeInfo.title as ITitle).english }}</h1>
+					<h1 class="native-title">{{ (animeInfo.title as ITitle).native }}</h1>
+				</div>
+			</div>
+			<div class="tabs-wrapper">
+				<div class="extra-info-tab-wrapper">
+					<h1 class="extra-info-title">Format:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item">
+							{{ animeInfo.type }}
+						</div>
+					</div>
+					<h1 class="extra-info-title">Studios:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item" v-for="studio in animeInfo.studios">
+							{{ studio }}
+						</div>
+					</div>
+					<h1 class="extra-info-title">Duration:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item">
+							{{ animeInfo.duration }}
+						</div>
+					</div>
+					<h1 class="extra-info-title">Episodes:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item">
+							{{ animeInfo.totalEpisodes }}
+						</div>
+					</div>
+					<h1 class="extra-info-title">Status:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item">
+							{{ animeInfo.status.toUpperCase() }}
+						</div>
+					</div>
+					<h1 class="extra-info-title">Released in:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item">
+							{{ animeInfo.releaseDate }}
+						</div>
+					</div>
+					<h1 class="extra-info-title">Season:</h1>
+					<div class="extra-info-item-wrapper">
+						<div class="extra-info-item">
+							{{ animeInfo.season }}
+						</div>
+					</div>
+				</div>
+				<div class="episode-tab-wrapper">
+					<p class="description" v-html="animeInfo.description"></p>
+					<h1 class="episode-text">Episodes</h1>
+					<div class="episode-list">
+						<div class="episode-wrapper" v-for="episode in episodeList" v-on:click="goToEpisode(episode)">
+							<img class="episode-image" :src="(episode.image as string)" alt="">
+							<div class="episode-info">
+								<h2 class="episode-title">{{ episode.title }}</h2>
+								<h2 class="episode-number">Episode {{ episode.number }}</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="mobile-spacer"></div>
+		</div>
 		<div class="bottom-nav-bar">
 			<div class="selected-tab"></div>
 			<ul class="navbar-list">
@@ -33,7 +83,7 @@
 				<li class="navbar-item" v-on:click="changeTab(3)">Related</li>
 			</ul>
 		</div>
-    </div>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -51,70 +101,76 @@ const ANILIST: Anilist = new META.Anilist(new ANIME.Zoro());
 const animeInfo: IAnimeInfo = await ANILIST.fetchAnilistInfoById(id);
 let episodeList = ref([]);
 const {
-    episodeError,
-    data: episodeResponse
+	episodeError,
+	data: episodeResponse
 } = await useFetch('https://consumet-api.herokuapp.com/meta/anilist/episodes/' + id + '?provider=zoro', { key: 'episodes' + id });
 
 function loadEpisodes() {
-    if (episodeResponse.value) {
-        episodeList.value = episodeResponse.value;
-    }
+	if (episodeResponse.value) {
+		episodeList.value = episodeResponse.value;
+	}
 }
 
 loadEpisodes()
 
 watch(episodeResponse, () => {
-    loadEpisodes()
+	loadEpisodes()
 });
 
 async function goToEpisode(episode: IAnimeEpisode) {
-    await navigateTo('/stream/' + animeInfo.id + '/' + ((animeInfo.title as ITitle).english ? (animeInfo.title as ITitle).english.toLowerCase().replaceAll(' ', '-') : (animeInfo.title as ITitle).native.toLowerCase().replaceAll(' ', '-')) + '-ep-' + episode.number);
+	await navigateTo('/stream/' + animeInfo.id + '/' + ((animeInfo.title as ITitle).english ? (animeInfo.title as ITitle).english.toLowerCase().replaceAll(' ', '-') : (animeInfo.title as ITitle).native.toLowerCase().replaceAll(' ', '-')) + '-ep-' + episode.number);
 }
 
 function changeTab(index) {
+	let tabWrapper = document.querySelector('.tabs-wrapper') as HTMLElement;
 	let currentTab = document.querySelector('.navbar-list').children[index] as HTMLElement;
-	let width = currentTab.offsetWidth - 8;
-	let x = currentTab.offsetLeft;
-	console.log({x});
+	let width = currentTab.offsetWidth + 2;
+	let x = currentTab.offsetLeft + 10;
+	console.log({ x });
 	let tabIndicator = document.querySelector('.selected-tab') as HTMLElement;
 	tabIndicator.style.width = width + 'px';
 	tabIndicator.style.paddingLeft = '0';
 	tabIndicator.style.paddingRight = '0';
 	tabIndicator.style.left = x + 'px';
+	if(index == 0) {
+		tabWrapper.style.transform = 'translateX(0%)';
+	} else if(index == 1) {
+		tabWrapper.style.transform = 'translateX(-100%)';
+	}
 }
 
 useHead({
-    title: `Info of ${(animeInfo.title as ITitle).english ?? (animeInfo.title as ITitle).native}`,
-    meta: [
-        {
-            name: "og:title",
-            content: `${(animeInfo.title as ITitle).english ?? (animeInfo.title as ITitle).native}`
-        },
-        {
-            name: "og:type",
-            content: "website"
-        },
-        {
-            name: "og:url",
-            content: `https://inu.watch/info/${animeInfo.id}`
-        },
-        {
-            name: "og:image",
-            content: animeInfo.cover || animeInfo.image
-        },
-        {
-            name: "og:description",
-            content: `${animeInfo.description}`
-        },
-        {
-            name: "twitter:card",
-            content: "summary_large_image"
-        },
-        {
-            name: "theme-color",
-            content: animeInfo.color ?? '#000000'
-        }
-    ]
+	title: `Info of ${(animeInfo.title as ITitle).english ?? (animeInfo.title as ITitle).native}`,
+	meta: [
+		{
+			name: "og:title",
+			content: `${(animeInfo.title as ITitle).english ?? (animeInfo.title as ITitle).native}`
+		},
+		{
+			name: "og:type",
+			content: "website"
+		},
+		{
+			name: "og:url",
+			content: `https://inu.watch/info/${animeInfo.id}`
+		},
+		{
+			name: "og:image",
+			content: animeInfo.cover || animeInfo.image
+		},
+		{
+			name: "og:description",
+			content: `${animeInfo.description}`
+		},
+		{
+			name: "twitter:card",
+			content: "summary_large_image"
+		},
+		{
+			name: "theme-color",
+			content: animeInfo.color ?? '#000000'
+		}
+	]
 });
 
 </script>
@@ -123,213 +179,274 @@ useHead({
 $mobile: 500px;
 
 .body {
-    background-color: #16151A;
-    width: calc(100vw - 8px);
-    height: 100vh;
-    margin: 0;
-    padding: 0;
+	background-color: #16151A;
+	width: calc(100vw - 8px);
+	height: 100vh;
+	margin: 0;
+	padding: 0;
 }
 
 .cover-image {
-    width: 100%;
-    height: calc(50vh - 1px);
-    object-fit: cover;
-    object-position: center;
-    transition: 0.3s all ease;
-    position: absolute;
+	width: 100%;
+	height: calc(50vh - 1px);
+	object-fit: cover;
+	object-position: center;
+	transition: 0.3s all ease;
+	position: absolute;
 
-    @media screen and (max-width: $mobile) {
-        height: 60vh;
-    }
+	@media screen and (max-width: $mobile) {
+		height: calc(60vh - 1px);
+	}
 }
 
 .cover-gradient {
-    width: 100%;
-    height: 50vh;
-    background: linear-gradient(180deg, rgba(22, 21, 26, 0) 0%, rgba(22, 21, 26, 1) 100%);
-    transition: 0.3s all ease;
-    position: absolute;
+	width: 100%;
+	height: 50vh;
+	background: linear-gradient(180deg, rgba(22, 21, 26, 0) 0%, rgba(22, 21, 26, 1) 100%);
+	transition: 0.3s all ease;
+	position: absolute;
 
-    @media screen and (max-width: $mobile) {
-        height: 60vh;
-    }
+	@media screen and (max-width: $mobile) {
+		height: 60vh;
+	}
 }
 
 .info-wrapper {
-    position: absolute;
-    width: 100%;
-    overflow-x: hidden;
+	position: absolute;
+	width: 100vw;
+	overflow-x: hidden;
 
-    & .top-wrapper {
-        position: relative;
-        height: 62vh;
-        color: white;
-        display: flex;
-        font-size: 20px;
-        flex-direction: row;
-        align-items: flex-end;
-        justify-content: flex-start;
-        margin-left: 60px;
-        transition: 0.3s all ease;
+	& .top-wrapper {
+		position: relative;
+		height: 62vh;
+		color: white;
+		display: flex;
+		font-size: 20px;
+		flex-direction: row;
+		align-items: flex-end;
+		justify-content: flex-start;
+		margin-left: 60px;
+		transition: 0.3s all ease;
 
-        & .poster-wrapper {
+		& .poster-wrapper {
 
-            & .poster-image {
-                width: 140px;
-                aspect-ratio: 0.64150943396;
-                object-fit: cover;
-                object-position: center;
-                border-radius: 20px;
-                margin-bottom: 20px;
-                margin-right: 20px;
-            }
-        }
+			& .poster-image {
+				width: 140px;
+				aspect-ratio: 0.64150943396;
+				object-fit: cover;
+				object-position: center;
+				border-radius: 20px;
+				margin-bottom: 20px;
+				margin-right: 20px;
+			}
+		}
 
-        & .title-wrapper {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            padding-bottom: 9vh;
+		& .title-wrapper {
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			padding-bottom: 9vh;
 
-            & .native-title {
-                font-size: 18px;
-                font-family: 'Zen Maru Gothic';
-            }
-        }
-
-
-
-        @media screen and (max-width: $mobile) {
-            font-size: 12px;
-            height: 68vh;
-            flex-direction: column;
-            align-items: center;
-            justify-content: flex-end;
-            margin-left: 0;
-
-            & .poster-wrapper {
-                & .poster-image {
-                    margin-right: 0;
-                }
-            }
+			& .native-title {
+				font-size: 18px;
+				font-family: 'Zen Maru Gothic';
+			}
+		}
 
 
-            & .title-wrapper {
-                align-items: center;
-                padding-bottom: 0;
 
-                & .native-title {
-                    font-size: 14px;
-                    font-family: 'Zen Maru Gothic';
-                }
-            }
-        }
-    }
+		@media screen and (max-width: $mobile) {
+			font-size: 12px;
+			height: 68vh;
+			flex-direction: column;
+			align-items: center;
+			justify-content: flex-end;
+			margin-left: 0;
 
-    & .description {
-        background-color: #1E222C;
-        padding: 16px;
-        text-align: start;
-        font-size: 16px;
-        margin: 30px;
-        border-radius: 20px;
-    }
+			& .poster-wrapper {
+				& .poster-image {
+					margin-right: 0;
+				}
+			}
 
-    & .episode-text {
-        font-size: 32px;
-        font-weight: bold;
-        margin-left: 30px;
-        margin-bottom: 20px;
-    }
 
-    & .episode-list {
-        display: flex;
-        margin-left: 30px;
-        margin-right: 30px;
-        flex-direction: row;
-        overflow-x: scroll;
+			& .title-wrapper {
+				align-items: center;
+				padding-bottom: 0;
 
-        & .episode-wrapper {
-            display: flex;
-            margin-bottom: 20px;
-            flex-direction: column;
+				& .native-title {
+					font-size: 14px;
+					font-family: 'Zen Maru Gothic';
+				}
+			}
+		}
+	}
 
-            & .episode-image {
-                width: 260px;
-                aspect-ratio: 16 / 9;
-                border-radius: 12px;
-                margin-right: 12px;
-            }
+	& .tabs-wrapper {
+		display: flex;
+		justify-content: flex-start;
+		transition: 0.4s all ease;
 
-            & .episode-info {
-                font-size: 10px;
-                width: 100%;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
+		& .extra-info-tab-wrapper {
+			width: 220px;
+			height: fit-content;
+			margin: 30px;
+			background-color: #1E222C;
+			padding: 20px;
+			border-radius: 12px;
 
-                & .episode-title {
-                    display: -webkit-box;
-                    -webkit-line-clamp: 2;
-                    line-clamp: 2;
-                    -webkit-box-orient: vertical;
-                    overflow-y: hidden;
-                }
+			& .extra-info-title {
+				width: 100%;
+				font-size: 18px;
+				margin-bottom: 10px;
+			}
 
-                & .episode-number {
-                    font-size: 12px;
-                    color: #999999;
-                    margin-top: 6px;
-                }
-            }
-        }
-    }
+			& .extra-info-item-wrapper {
+				& .extra-info-item {
+					width: 62vw;
+					margin-left: 20px;
+					padding: 6px 12px;
+					margin-bottom: 10px;
+					border-radius: 10px;
+					background-color: transparent;
+					transition: 0.3s all ease;
 
-    @media screen and (max-width: $mobile) {
-        & .description {
-            text-align: center;
-            font-size: 14px;
-            border-radius: 12px;
-        }
+					&:hover {
+						background-color: #16151ab8;
+					}
+				}
+			}
+		}
+		& .episode-tab-wrapper {
+			width: 70vw;
+			margin: 30px;
+			& .description {
+				background-color: #1E222C;
+				padding: 16px;
+				text-align: start;
+				font-size: 16px;
+				margin-bottom: 30px;
+				border-radius: 20px;
+			}
 
-        & .episode-text {
-            font-size: 24px;
-        }
+			& .episode-text {
+				font-size: 32px;
+				font-weight: bold;
+				margin-bottom: 20px;
+			}
 
-        & .episode-list {
-            flex-direction: column;
-            margin-bottom: 0;
-            overflow-x: hidden;
+			& .episode-list {
+				width: 100%;
+				display: flex;
+				margin-right: 30px;
+				flex-direction: row;
+				overflow-x: scroll;
 
-            & .episode-wrapper {
-                flex-direction: row;
+				& .episode-wrapper {
+					display: flex;
+					margin-bottom: 20px;
+					flex-direction: column;
 
-                & .episode-image {
-                    width: 126px;
-                }
-            }
-        }
-    }
+					& .episode-image {
+						width: 260px;
+						aspect-ratio: 16 / 9;
+						border-radius: 12px;
+						margin-right: 12px;
+					}
+
+					& .episode-info {
+						font-size: 10px;
+						width: 100%;
+						display: flex;
+						flex-direction: column;
+						justify-content: center;
+
+						& .episode-title {
+							display: -webkit-box;
+							-webkit-line-clamp: 2;
+							line-clamp: 2;
+							-webkit-box-orient: vertical;
+							overflow-y: hidden;
+						}
+
+						& .episode-number {
+							font-size: 12px;
+							color: #999999;
+							margin-top: 6px;
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+
+	@media screen and (max-width: $mobile) {
+		& .tabs-wrapper {
+			transform: translateX(-100%);
+				
+			& .extra-info-tab-wrapper {
+				width: 100vw;
+			}
+
+			& .episode-tab-wrapper {
+				width: 100vw;
+				& .description {
+				width: 78vw;
+					text-align: center;
+					font-size: 14px;
+					border-radius: 12px;
+				}
+
+				& .episode-text {
+					font-size: 24px;
+				}
+
+				& .episode-list {
+					flex-direction: column;
+					margin-bottom: 0;
+					overflow-x: hidden;
+
+					& .episode-wrapper {
+						flex-direction: row;
+
+						& .episode-image {
+							width: 126px;
+						}
+					}
+				}
+			}
+		}
+
+
+	}
+}
+
+.mobile-spacer {
+
+	height: 80px;
 }
 
 .bottom-nav-bar {
 	position: fixed;
-	bottom: 0;
+	bottom: -100px;
 	left: 0;
-	width: calc(100vw - 40px);
+	width: calc(100vw - 60px);
 	height: 70px;
 	margin: 20px;
 	background-color: black;
 	border-radius: 12px;
+	padding-right: 20px;
+	transition: 0.3s all ease;
 
 	& .selected-tab {
 		position: absolute;
 		background-color: #1E222C;
-		width: 80px;
-		height: 36px;
-		left: 0;
-		margin-top: 17px;
-		margin-left: 14px;
+		width: 20%;
+		height: 50%;
+		left: calc(12px + 25%);
+		top: 25%;
 		border-radius: 18px;
 		transition: 0.3s all ease;
 	}
@@ -343,13 +460,22 @@ $mobile: 500px;
 		align-items: center;
 		height: 100%;
 		margin: 0 10px;
+		width: calc(100vw - 60px);
 
 		& .navbar-item {
 			background-color: transparent;
-			padding: 10px 16px;
 			border-radius: 50px;
 			font-size: 3vw;
+			width: 20%;
+			height: 50%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
 		}
+	}
+
+	@media screen and (max-width: $mobile) {
+		bottom: 0;
 	}
 }
 </style>
